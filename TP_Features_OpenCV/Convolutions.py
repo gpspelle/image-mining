1789158f645375c6f833c2f824b2fa0ec790d70f
damounayman <aymandamoun@gmail.com>
2020-09-26 17:56:01 +0200

    comment Convolution.py

--- TP_Features_OpenCV/Convolutions.py
+++ TP_Features_OpenCV/Convolutions.py
@@ -1,6 +1,6 @@
+# import library
 import numpy as np
 import cv2
-
 from matplotlib import pyplot as plt
 
 #Read grayscale image and conversion to float64
@@ -10,12 +10,15 @@ print("Image dimension:",h,"rows x",w,"columns")
 
 #Direct method
 t1 = cv2.getTickCount()
-img2 = cv2.copyMakeBorder(img,0,0,0,0,cv2.BORDER_REPLICATE)
+#Code execution
+img2 = cv2.copyMakeBorder(img,0,0,0,0,cv2.BORDER_REPLICATE) #Adding borders
 for y in range(1,h-1):
   for x in range(1,w-1):
+    #perform a convolution operation with a 3×3 filter
     val = 5*img[y, x] - img[y-1, x] - img[y, x-1] - img[y+1, x] - img[y, x+1] 
     img2[y,x] = min(max(val,0),255)
 t2 = cv2.getTickCount()
+#Find the time of execution in seconds
 time = (t2 - t1)/ cv2.getTickFrequency()
 print("Direct method:",time,"s")
 
@@ -31,6 +34,7 @@ t2 = cv2.getTickCount()
 time = (t2 - t1)/ cv2.getTickFrequency()
 print("Method filter2D :",time,"s")
 
+#plotting the figures
 plt.subplot(312)
 plt.imshow(img3,cmap = 'gray',vmin = 0.0,vmax = 255.0)
 plt.title('Convolution - filter2D')
